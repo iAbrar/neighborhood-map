f776ccf7274770bf6a4bfd8e2892d3ff1c0c8b59
iAbrar <abrar.ksu@gmail.com>
2017-05-22 00:29:42 +0300

    start from scratch

--- js/app.js
+++ js/app.js
@@ -1,172 +1,165 @@
 'use strict';
 
-// Create marker for the location "Model"
-var initialLocation = [
-{
-	"name":"Nino",
-	"LatLng":{lat:24.698162 ,lng:46.686809},
-	"category": "Restaurant"
-},
-{
-	"name":"Centria Mall",
-	"LatLng":{lat:24.697509,lng:46.683966},
-	"category": "Shopping"
-},
-{
-	"name":"Al Faisaliah Mall",
-	"LatLng":{lat:24.689536,lng:46.685833},
-	"category": "Shopping"
-},
-{
-	"name":"Café Bateel",
-	"LatLng":{lat:24.699741,lng:46.690741},
-	"category": "Cafe"
-},
-{
-	"name":"Diamond Restaurant",
-	"LatLng":{lat:24.699542,lng:46.692356},
-	"category": "Restaurant"
-},
-{
-	"name":"Tim Hortons",
-	"LatLng":{lat:24.69866,lng:46.689851},
-	"category": "Cafe"
-},
-{
-	"name":"Bab Al-Yemen Restaurant",
-	"LatLng":{lat:24.693966,lng:46.678768},
-	"category": "Restaurant"
-},
-{
-	"name":"Panorama Mall",
-	"LatLng":{lat:24.692129,lng:46.670512},
-	"category": "Shopping"
-}
-];
-// create Location class
-var Location = function(data){
-this.name = ko.observable(data.name);
-this.LatLng = ko.observable(data.LatLng);
-this.category = ko.observable(data.category);
-this.visible = ko.observable(true);
+// Create marker for the location "Data Model"
+//**********************
+//		Locations
+//**********************
 
-}
-            // add markers of locations on the map
-  function addMarkers(locations,map) {
-    // The following group uses the location array to create an array of markers on initialize.
-        for (var i = 0; i < locations.length; i++) {
+var Locations = [{
+        "name": "Nino",
+        "LatLng": {
+            lat: 24.698162,
+            lng: 46.686809
+        },
+        "category": "Restaurant"
+    },
+    {
+        "name": "Centria Mall",
+        "LatLng": {
+            lat: 24.697509,
+            lng: 46.683966
+        },
+        "category": "Shopping"
+    },
+    {
+        "name": "Al Faisaliah Mall",
+        "LatLng": {
+            lat: 24.689536,
+            lng: 46.685833
+        },
+        "category": "Shopping"
+    },
+    {
+        "name": "Café Bateel",
+        "LatLng": {
+            lat: 24.699741,
+            lng: 46.690741
+        },
+        "category": "Cafe"
+    },
+    {
+        "name": "Diamond Restaurant",
+        "LatLng": {
+            lat: 24.699542,
+            lng: 46.692356
+        },
+        "category": "Restaurant"
+    },
+    {
+        "name": "Tim Hortons",
+        "LatLng": {
+            lat: 24.69866,
+            lng: 46.689851
+        },
+        "category": "Cafe"
+    },
+    {
+        "name": "Bab Al-Yemen Restaurant",
+        "LatLng": {
+            lat: 24.693966,
+            lng: 46.678768
+        },
+        "category": "Restaurant"
+    },
+    {
+        "name": "Panorama Mall",
+        "LatLng": {
+            lat: 24.692129,
+            lng: 46.670512
+        },
+        "category": "Shopping"
+    }
+];
+//initialize map 
+//**********************
+//		Map
+//**********************
+
+var map;
+// Create a new blank array for all the listing markers.
+var markers = [];
+
+
+// Constructor creates a new map - only center and zoom are required.
+
+ function initMap() {
+        // Create a styles array to use with the map.
+     
+
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 24.697285134978586, lng: 46.685779094696045},
+          zoom: 16,
+          mapTypeControl: false
+        });
+
+  
+
+        var largeInfowindow = new google.maps.InfoWindow();
+
+        
+        // The following group uses the location array to create an array of markers on initialize.
+        for (var i = 0; i < Locations.length; i++) {
           // Get the position from the location array.
-          var position = locations[i].LatLng;
-          var title = locations[i].name;
+          var position = Locations[i].location;
+          var title = Locations[i].title;
           // Create a marker per location, and put into markers array.
           var marker = new google.maps.Marker({
-            map: map,
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
+         //  icon: defaultIcon,
             id: i
           });
           // Push the marker to our array of markers.
           markers.push(marker);
-          // Create an onclick event to open an infowindow at each marker.
+          // Create an onclick event to open the large infowindow at each marker.
           marker.addListener('click', function() {
             populateInfoWindow(this, largeInfowindow);
           });
-          bounds.extend(markers[i].position);
+          // Two event listeners - one for mouseover, one for mouseout,
+          // to change the colors back and forth.
+          marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+          });
+          marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+          });
         }
-        console.log(locations.length);
+       
+
+      }
+// create Location class
+//**********************
+//		Location
+//**********************
+var Location = function(data) {
+    this.name = ko.observable(data.name);
+    this.LatLng = ko.observable(data.LatLng);
+    this.category = ko.observable(data.category);
+
 }
+
 
 // the viewModel
-var ViewModel= function () {
-var self =this;
- this.filterSearch = ko.observable("");
-
-this.locationList = ko.observableArray([]);
-
-    initialLocation.forEach(function(locationItem){
-        self.locationList.push( new Location(locationItem) );
-    });
+//**********************
+//		View Model
+//**********************
+var ViewModel = function() {
+    var self = this;
 
 
-//initialize map 
- var map;
-       // Create a new blank array for all the listing markers.
-      var markers = [];
-       var largeInfowindow = new google.maps.InfoWindow();
-        var bounds = new google.maps.LatLngBounds();
-
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
-	                    center: {
-                        lat: 24.697285134978586,
-                        lng: 46.685779094696045
-                    },
-                    zoom: 16
-                });    
-	
-this.filteredList = ko.computed( function() {
-		var filter = self.filterSearch().toLowerCase();
-		if (!filter) {
-			self.locationList().forEach(function(locationItem){
-				locationItem.visible(true);
-			});
-			return self.locationList();
-		} else {
-			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
-				var string = locationItem.name.toLowerCase();
-				var result = (string.search(filter) >= 0);
-				locationItem.visible(result);
-				return result;
-			});
-		}
-	}, self);
-               
+}; //end view
 
 
-  function addMarkers(locations,map) {
-    // The following group uses the location array to create an array of markers on initialize.
-        for (var i = 0; i < locations.length; i++) {
-          // Get the position from the location array.
-          var position = locations[i].LatLng;
-          var title = locations[i].name;
-          // Create a marker per location, and put into markers array.
-          var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
-          });
-          // Push the marker to our array of markers.
-          markers.push(marker);
-          // Create an onclick event to open an infowindow at each marker.
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-          });
-          bounds.extend(markers[i].position);
-        }
-}
+// *******************************
+// *            SETUP            *
+// *******************************
 
-      // This function populates the infowindow when the marker is clicked. We'll only allow
-      // one infowindow which will open at the marker that is clicked, and populate based
-      // on that markers position.
-      function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-          });
-        }
-      }
-addMarkers(initialLocation,map);
-};//end view
+// empty view model
+var vm = new ViewModel();
 
-function loadScript(){
-	ko.applyBindings(new ViewModel);
+// listener for view model initialization
 
-}
+// listener for google map initialization
+//google.maps.event.addDomListener(window, 'load', initMap());
